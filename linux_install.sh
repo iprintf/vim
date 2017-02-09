a4a5d671cc2b546ce562daec874d5784be2a313a
kyo <iprintf@qq.com>
2017-02-09 15:31:14 +0800

    修改linux安装脚本使用软链接方式安装

--- linux_install.sh
+++ linux_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-install_dir=~/.kyo
+install_dir=$(pwd)/config
 
 function err_exit() {
     echo $1
@@ -8,8 +8,8 @@ function err_exit() {
 }
 
 function php_manual() {
-    vimDocPath=$install_dir/vim/.vim/bundle/vim-misc/
-    vimDocFile=$install_dir/vim/.vim/bundle/vim-misc/doc.tar.bz2
+    vimDocPath=$install_dir/.vim/bundle/vim-misc/
+    vimDocFile=$install_dir/.vim/bundle/vim-misc/doc.tar.bz2
     test -e "$vimDocFile" && tar -xf $vimDocFile -C $vimDocPath
 }
 
@@ -21,15 +21,17 @@ test -e ~/.vimrc -o -e ~/.vimrc.local -o -e ~/.vim -o -e ~/.vim.plugins \
 
 rm ~/.vim* -rf &> /dev/null
 
-test ! -e $install_dir && mkdir $install_dir
+# test ! -e $install_dir && mkdir $install_dir
 
-test -e $install_dir/vim && rm $install_dir/vim -rf
+# test -e $install_dir/vim && rm $install_dir/vim -rf
 
-/bin/cp config $install_dir/vim -rf || err_exit "安装失败"
+# /bin/cp config $install_dir/vim -rf || err_exit "安装失败"
 php_manual
 
-ln -s $install_dir/vim/.vim ~/.vim
-ln -s $install_dir/vim/.vimrc ~/.vimrc
-ln -s $install_dir/vim/.vimrc.local ~/.vimrc.local
-ln -s $install_dir/vim/.vimrc.plugins ~/.vimrc.plugins
+ln -s $install_dir/.vim ~/.vim
+ln -s $install_dir/.vimrc ~/.vimrc
+ln -s $install_dir/.vimrc.local ~/.vimrc.local
+ln -s $install_dir/.vimrc.plugins ~/.vimrc.plugins
 
+echo '安装成功!'
+echo '提示: ~/kyo_old_vim.*.tar为备份安装前的vim配置，请自行处理!'
